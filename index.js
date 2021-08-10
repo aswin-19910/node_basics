fc3c9180d600da8a6593914e077aece379eff525
Aswin Kumar <aswin.kumar.17@mountblue.tech>
2021-08-10 10:22:47 +0530

    Modifying OTP Output message

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var otpGenerator = require('otp-generator')
 
-console.log(otpGenerator.generate(6, { digits: true, alphabets: false,upperCase: false, specialChars: false }));
+console.log(`Your OTP:`,otpGenerator.generate(6, { digits: true, alphabets: false,upperCase: false, specialChars: false }));
