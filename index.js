518c2ba4949687a73326f581521393016704876c
Aswin Kumar <aswin.kumar.17@mountblue.tech>
2021-08-10 10:16:39 +0530

    Modified index.js
    First line has been deleted

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("hello world");
 var otpGenerator = require('otp-generator')
 
 console.log(otpGenerator.generate(6, { digits: true, alphabets: false,upperCase: false, specialChars: false }));
